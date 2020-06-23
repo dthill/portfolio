30706fb072dc0b0bda7b9087fc820c5fa5f7e888
D <d>
2020-06-23 11:03:15 +0200

    refactor css: use sass and bem

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,33 @@
-
-const editButton = document.getElementsByClassName("edit-button")[0];
-const backgroundText = document.getElementsByClassName("background-text")[0];
-const bannerText = document.getElementsByClassName("banner-text")[0];
-const bannerArrow = document.getElementsByClassName("banner-arrow")[0];
-const originalHTMLContent = document.getElementsByClassName("main")[0].innerHTML.replace(/(\r\n|\n|\r)/gm,"");
-const mainSection = document.getElementsByClassName("main")[0];
-
+const editButton = document.getElementById("edit-button");
+const backgroundText = document.getElementById("background-text");
+const bannerText = document.getElementById("banner-text");
+const bannerArrow = document.getElementById("banner-arrow");
+const originalHTMLContent = document
+  .getElementById("main")
+  .innerHTML.replace(/(\r\n|\n|\r)/gm, "");
+const mainSection = document.getElementById("main");
 
 backgroundText.innerText = originalHTMLContent;
 
-function edit(){
-	backgroundText.innerText = originalHTMLContent;
-	mainSection.innerHTML = originalHTMLContent;
-	editButton.innerText = editButton.innerText === "Hack it!" ? "Cancel!" : "Hack it!";
-	editButton.classList.toggle("white");
-	backgroundText.classList.toggle("background-text-edit");
-	bannerText.classList.toggle("banner-text-hide");
-	bannerArrow.classList.toggle("banner-arrow-hide");
+function edit() {
+  backgroundText.innerText = originalHTMLContent;
+  mainSection.innerHTML = originalHTMLContent;
+  editButton.innerText =
+    editButton.innerText === "Hack it!" ? "Cancel!" : "Hack it!";
+  editButton.classList.toggle("navbar__item--invert");
+  backgroundText.classList.toggle("banner__background-text--edit");
+  bannerText.classList.toggle("banner__title-text--hidden");
+  bannerArrow.classList.toggle("banner__arrow--hide");
 }
 
 editButton.addEventListener("click", edit);
 
-document.addEventListener("keydown", function(event){
-	if(event.key === "Escape"){
-		edit();
-	}
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    edit();
+  }
 });
 
-backgroundText.addEventListener("keyup", function(event){
-	mainSection.innerHTML = this.innerText;
+backgroundText.addEventListener("keyup", function (event) {
+  mainSection.innerHTML = this.innerText;
 });
